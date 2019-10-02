077b8a86fe4292149e6c932ee463154d9dca6b5f
alexeysurmach <flashhome1985@gmail.com>
2019-10-02 21:33:33 +0300

    improved structure, highlighted button

--- src/App.js
+++ src/App.js
@@ -6,22 +6,21 @@ import TodoListFooter from './TodoListFooter';
 
 
 class App extends React.Component {
-     tasks = [
+    tasks = [
         {title: 'JS', isDone: true, priority: 'hi'},
         {title: 'HTML', isDone: false, priority: 'hi'},
         {title: 'CSS', isDone: false, priority: 'low'},
-        {title: 'React', isDone: false, priority: 'low' },
+        {title: 'React', isDone: false, priority: 'low'},
 
     ];
-     filterValue = 'Active';
-
+    filterValue = 'Active';
     render = () => {
         return (
             <div className="App">
                 < div className="todoList">
                     < TodoListHeader/>
-                    < TodoListTasks tasks ={this.tasks} />
-                    < TodoListFooter filterValue={this.filterValue} />
+                    < TodoListTasks tasks={this.tasks}/>
+                    < TodoListFooter filterValue={this.filterValue}/>
                 </div>
             </div>
         );
