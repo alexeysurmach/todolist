ea18ed03963a49e3625e52a22682b5cfad9e2537
alexeysurmach <flashhome1985@gmail.com>
2019-10-08 21:18:39 +0300

    add state and onclick

--- src/App.js
+++ src/App.js
@@ -6,21 +6,55 @@ import TodoListFooter from './TodoListFooter';
 
 
 class App extends React.Component {
-    tasks = [
-        {title: 'JS', isDone: true, priority: 'high'},
-        {title: 'HTML', isDone: false, priority: 'high'},
-        {title: 'CSS', isDone: false, priority: 'low'},
-        {title: 'React', isDone: true, priority: 'high'},
+    constructor(props) {
+        super(props);
+        this.newTaskTitleRef = React.createRef()
+        // setTimeout(
+        //     () => {
+        //         let newTask = {title: 'Python', isDone: true, priority: 'high'};
+        //         let newTasks = [...this.state.tasks, newTask];
+        //         this.setState({
+        //             tasks: newTasks
+        //         })
+        //     }, 3000
+        // );
+    };
 
-    ];
-    filterValue = 'All';
+    state = {
+        tasks: [
+            {title: 'JS', isDone: true, priority: 'high'},
+            {title: 'HTML', isDone: false, priority: 'high'},
+            {title: 'CSS', isDone: false, priority: 'low'},
+            {title: 'React', isDone: true, priority: 'high'},
+        ],
+        filterValue: 'All'
+
+    };
+
+    onAddTaskClick = () => {
+        let newTask = {title: this.newTaskTitleRef.current.value, priority: 'high', isDone: false};
+        this.newTaskTitleRef.current.value = '';
+        let newTasks = [...this.state.tasks, newTask];
+        this.setState({
+            tasks: newTasks
+        })
+    };
+
     render = () => {
         return (
             <div className="App">
                 < div className="todoList">
-                    < TodoListHeader/>
-                    < TodoListTasks tasks={this.tasks}/>
-                    < TodoListFooter filterValue={this.filterValue}/>
+                    <div className="todoList-header">
+                        <h3 className="todoList-header__title">What to Learn</h3>
+                        <div className="todoList-newTaskForm">
+                            <input ref={this.newTaskTitleRef = React.createRef()} type="text"
+                                   placeholder="New task name"/>
+                            <button onClick={this.onAddTaskClick}>Add</button>
+                        </div>
+                    </div>
+                    {/*< TodoListHeader/>*/}
+                    < TodoListTasks tasks={this.state.tasks}/>
+                    < TodoListFooter filterValue={this.state.filterValue}/>
                 </div>
             </div>
         );
